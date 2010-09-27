5e63a7689f303af176643f6bdcde781ec7f16e41
Peter Biro <pettair@gmail.com>
2010-09-27 12:19:08 +0200

    Fasz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd8bf9d25819aee8353a7fcc1b7d3a7a449a10893e285862e83063e9df903149f64b6898c73ac1f83bc19bb7027b59a2e3524b24cd5a9635dcdb3466a5636b936'
